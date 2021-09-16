a5a98199822c93e0c347348420eed26418aa1f59
Varad Gundap <30592638+varad615@users.noreply.github.com>
2021-09-16 15:08:09 +0530

    form validation

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,21 @@
 function onSubmit() {
-    console.log("welcome")
+  var validate = document.forms["loginform"]["username"].value;
+  var passvalidate = document.forms["loginform"]["password"].value;
+  if (
+    validate == "" ||
+    validate == null ||
+    passvalidate == "" ||
+    passvalidate == null
+  ) {
+    document.getElementById("demo").innerHTML =
+      "Please fill out all the fields";
+  } else {
+    console.log("user named " + validate + " loggedin");
+    alert("Welcome " + validate + ". Press ok to redirect");
+    window.location.href = "https://github.com/varad615";
+  }
+}
+
+function signup() {
+  console.log("welcome");
 }
